664d59e1d5cd6b56c9d2670bf0df82ad7b965e1c
Daffa Takarai <40546955+DaffaTakarai@users.noreply.github.com>
2022-09-26 19:24:05 +0100

    Add files via upload

---
+++ Oke.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oke;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author daffa
+ */
+public class Oke {
+public static Scanner input;
+   public static void main(String []args){
+       
+    
+       System.out.println("NAMA : SHIVA");
+       
+       System.out.println("NPM : 21552011076");
+      
+       
+      
+   }
+}
