e780fcda721f1ad3c0bcb6078b6dff48bea9141d
donzid <41707990+donzid@users.noreply.github.com>
2022-10-09 18:11:56 +0100

    Create jsonstring.js

---
+++ js/jsonstring.js
@@ -0,0 +1,10 @@
+<script>
+// Sample JS object
+var obj = {name: "Martin", age: 30, country: "United States"};
+    
+// Converting JS object to JSON string
+var json = JSON.stringify(obj);
+    
+console.log(json);
+// Prints: {"name":"Martin","age":30,"country":"United States"}
+</script>
